4ae460e66ccd532cf032dcc52ffa2002370bed6e
unknown <marcosaureliofarias2023@gmail.com>
2022-12-01 22:14:49 -0300

    teste

---
+++ src/main/config/module-alias.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var module_alias_1 = require("module-alias");
+var path_1 = require("path");
+(0, module_alias_1.addAlias)('@', (0, path_1.resolve)('dist'));
---
+++ src/tests/index.spec.js
@@ -0,0 +1,5 @@
+describe('', function () {
+    it('', function () {
+        expect(1).toBe(1);
+    });
+});
